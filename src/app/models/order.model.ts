05648d90793bfcda301d5ac3c3bb8ad96787af8e
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-05-10 10:54:53 -0300

    Stored checkout step data into session's browser

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,4 +1,7 @@
 import {Injectable} from '@angular/core';
+import {NavigationStart, Router} from '@angular/router';
+
+import 'rxjs/add/operator/filter';
 
 import {Repository} from './repository';
 import {Cart} from './cart.model';
@@ -7,7 +10,31 @@ import {Cart} from './cart.model';
 export class Order {
 
   constructor(private repo: Repository,
-              public cart: Cart) {
+              public cart: Cart,
+              router: Router) {
+    router.events
+      .filter(event => event instanceof NavigationStart)
+      .subscribe(() => {
+        if (router.url.startsWith('/checkout') && this.name != null && this.address != null) {
+          repo.storeSessionData('checkout', {
+            name: this.name,
+            address: this.address,
+            cardNumber: this.payment.cardNumber,
+            cardExpiry: this.payment.cardExpiry,
+            cardSecurityCode: this.payment.cardSecurityCode
+          });
+        }
+      });
+
+    const data = repo.getSessionData('checkout');
+
+    if (data) {
+      this.name = data.name;
+      this.address = data.address;
+      this.payment.cardNumber = data.cardNumber;
+      this.payment.cardExpiry = data.cardExpiry;
+      this.payment.cardSecurityCode = data.cardSecurityCode;
+    }
   }
 
   orderId: number;
